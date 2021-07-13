9ee2171879a0ad1cea60844775806294c43b6f6b
Alvaro <agalanteperez@gmail.com>
2021-07-13 11:29:38 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ejercicio1
+{
+    class Program
+    {
+        static int Main()
+        {
+            int a = 20;
+            int b = 5;
+           
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+
+            return 0;
+
+        }
+    }
+}
